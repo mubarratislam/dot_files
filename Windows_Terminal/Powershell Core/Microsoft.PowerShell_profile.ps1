1b5368b306be30c5a7f4b0865da348f7d10e04c3
mubarratislam <mdmubarrat@gmail.com>
2022-11-01 01:31:55 +0600

    Add touch, grep, unzip of CT.

--- Windows_Terminal/Powershell Core/Microsoft.PowerShell_profile.ps1
+++ Windows_Terminal/Powershell Core/Microsoft.PowerShell_profile.ps1
@@ -772,3 +772,31 @@ function uptime {
 function reload-profile {
         & $profile
 }
+
+function find-file($name) {
+    Get-ChildItem -recurse -filter "*${name}*" -ErrorAction SilentlyContinue | ForEach-Object {
+            $place_path = $_.directory
+            Write-Output "${place_path}\${_}"
+    }
+}
+
+# Unzipping file
+function unzip ($file) {
+    Write-Output("Extracting", $file, "to", $pwd)
+$fullFile = Get-ChildItem -Path $pwd -Filter .\cove.zip | ForEach-Object{$_.FullName}
+    Expand-Archive -Path $fullFile -DestinationPath $pwd
+}
+
+# Grep
+function grep($regex, $dir) {
+    if ( $dir ) {
+            Get-ChildItem $dir | select-string $regex
+            return
+    }
+    $input | select-string $regex
+}
+
+# Touch
+function touch($file) {
+    "" | Out-File $file -Encoding ASCII
+}
